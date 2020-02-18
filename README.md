b29863d101cbd5742955420c80f0543a31dee069
Edwin Martinez <martinez92edwin@gmail.com>
2020-02-17 17:24:44 -0800

    starter, created workspace and fist pacakge

---
+++ README.md
@@ -0,0 +1,5 @@
+# GO Chase It!!!!
+
+This project was created usign Ubuntu 16.04 LTS along with ROS 1 and Gazebo
+
+
---
+++ build/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/edwin/Documents/Projects/GO_Chase_It!/src/my_robot
+# Build directory: /home/edwin/Documents/Projects/GO_Chase_It!/build/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
