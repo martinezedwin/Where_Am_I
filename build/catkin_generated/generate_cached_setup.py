7d63a56eaf96f0b977cff4379d29fed4435bcd16
Edwin Martinez <martinez92edwin@gmail.com>
2020-02-19 21:51:22 -0800

    added image processing node, still needs work

--- build/catkin_generated/generate_cached_setup.py
+++ build/catkin_generated/generate_cached_setup.py
@@ -12,7 +12,7 @@ try:
     from catkin.environment_cache import generate_environment_script
 except ImportError:
     # search for catkin package in all workspaces and prepend to path
-    for workspace in "/home/edwin/Documents/Projects/Go_Chase_It/devel;/home/edwin/Documents/Projects/simple_arm_mover_catkin_ws/devel;/opt/ros/kinetic".split(';'):
+    for workspace in "/home/edwin/Documents/Projects/Go_Chase_It/devel;/opt/ros/kinetic".split(';'):
         python_path = os.path.join(workspace, 'lib/python2.7/dist-packages')
         if os.path.isdir(os.path.join(python_path, 'catkin')):
             sys.path.insert(0, python_path)
